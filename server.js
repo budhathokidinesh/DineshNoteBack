88e8a30c2d2e6464b2801363ed8e83afd91f709b
Dinesh Budhathoki <physmarika@gmail.com>
2025-10-26 23:34:35 +0800

    rag continues

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const app = express();
 //DB connection and server status
 dbConnect()
   .then(() => {
-    /** ready to use. The `mongoose.connect()` promise resolves to mongoose
-    instance. */
     app.listen(PORT, "0.0.0.0", (error) => {
       return !error
         ? console.log(`server is running at http://localhost:${PORT}`)
